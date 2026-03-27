4f8030ea005398ce1682e2d8454aacdf838451f8
Mishanya00 <polyd@tut.by>
2026-03-27 09:55:38 +0300

    feat: Round Robin Molchanov L4

---
+++ week_2_concurrency_mastering/molchanov_playlist/3_generators.py
@@ -0,0 +1,42 @@
+from time import time
+
+
+def gen_filename():
+    while True:
+        pattern = 'file-{}.jpeg'
+        t = int(time() * 1000)
+        yield pattern.format(str(t))
+
+        sum = 234 + 234
+        print(sum)
+
+
+# g = gen_filename()
+
+
+def gen1(s):
+    for i in s:
+        yield i
+
+
+def gen2(n):
+    for i in range(n):
+        yield i
+
+
+g1 = gen1('michael')
+g2 = gen2(4)
+
+tasks = [g1, g2]
+
+
+# Round Robin - Карусель задач
+while tasks:
+    task = tasks.pop(0)
+
+    try:
+        i = next(task)
+        print(i) # Some task here
+        tasks.append(task)
+    except StopIteration:
+        pass
